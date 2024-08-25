5a8d65df2a59930e907acd7bddaee908a7acf8d0
Galaxy <feder2019@icloud.com>
2024-08-25 15:32:28 +0200

    Delete script.js

--- script.js
+++
@@ -1,8 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const nav = document.querySelector('nav');
-    const toggle = document.querySelector('.toggle');
-
-    toggle.addEventListener('click', function() {
-        nav.classList.toggle('active');
-    });
-});
