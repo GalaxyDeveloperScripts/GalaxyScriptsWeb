a90d16fb557ce119d4b9be73dc5b1e907633898b
Galaxy <feder2019@icloud.com>
2024-08-29 15:15:06 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
-// Script per gestire l'hamburger menu
-document.addEventListener("DOMContentLoaded", function() {
-    const toggleButton = document.querySelector('.toggle-button');
-    const navLinks = document.querySelector('.links');
-
-    toggleButton.addEventListener('click', () => {
-        navLinks.classList.toggle('active');
-    });
-});
+const wrapper = document.querySelector('.wrapper');
+const loginLink = document.querySelector('.login-link');
+const registerLink = document.querySelector('.register-link');
+const btnPopup = document.querySelector('.btnLogin-popup');
+const iconClose = document.querySelector('.icon-close');
